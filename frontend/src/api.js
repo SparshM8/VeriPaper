53ea6c9e746a604787c6ec5f725b22f9af520a16
SparshM8 <callme8samay@gmail.com>
2026-02-01 13:46:57 +0530

    Update frontend API baseURL to deployed Render URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://veripaper.onrender.com/api",
 });
 
 export async function analyzePaper(file) {
